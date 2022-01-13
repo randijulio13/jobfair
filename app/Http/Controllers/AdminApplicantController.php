be08c01b5a88499e0e481e77d2bef487be31e806
randijulio13 <randijulio13@gmail.com>
2022-01-13 17:24:51 +0700

    update datatable pelamar

--- app/Http/Controllers/AdminApplicantController.php
+++ app/Http/Controllers/AdminApplicantController.php
@@ -34,14 +34,14 @@ class AdminApplicantController extends Controller
 
     function datatables()
     {
-        $sponsor = DB::table('users as u')->select('u.*', 's.id as sponsor_id', 's.type as sponsor_type')->join('sponsors as s', 's.user_id', '=', 'u.id')->where('u.id', '=', session('userdata')['id'])->first();
-        $sponsor_id = $sponsor->sponsor_id;
         $data = DB::table('applicant_fields as af')
             ->select('ad.*', 'u.phone', 'u.email')
             ->join('applicant_datas as ad', 'ad.id', '=', 'af.applicant_id')
             ->join('users as u', 'u.id', '=', 'ad.user_id')
             ->where('u.status', '=', 1);
         if (session('userdata')['type'] != 1) {
+            $sponsor = DB::table('users as u')->select('u.*', 's.id as sponsor_id', 's.type as sponsor_type')->join('sponsors as s', 's.user_id', '=', 'u.id')->where('u.id', '=', session('userdata')['id'])->first();
+            $sponsor_id = $sponsor->sponsor_id;
             $data = $data->where('ad.file', '!=', null);
             $fields = DB::table('sponsor_fields as sf')
                 ->where('sf.sponsor_id', '=', $sponsor_id)
